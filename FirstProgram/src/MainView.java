257662a1c2548eafe90dcaa818b96b7ca8f607c8
ACER <ACER@Vaishnavi-Shinde>
2022-08-08 14:17:31 +0530

    Assignment 1

---
+++ FirstProgram/src/MainView.java
@@ -0,0 +1,9 @@
+
+public class MainView {
+	public static void main(String[] args) {
+		
+		int myNum = 32568;
+		System.out.println(myNum);
+	}
+
+}
---
+++ SampleProject/src/org/example/model/Customer.java
@@ -0,0 +1,8 @@
+package org.example.model;
+
+public class Customer {
+	
+	int customerId;
+	int custId=1;
+
+}
